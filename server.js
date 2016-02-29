d8e980e68f3d07577a3e350d0236cde0388bc1c4
José Moreira <jose.moreira@findhit.com>
2016-02-29 19:04:18 +0000

    Use SERVER_API_PREFIX

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var env = process.env
 
 var REDIS_URI = env.REDIS_URI || 'redis://localhost:6379'
 var HTTP_PORT = env.SERVER_API_PORT || 8080
-var HTTP_PREFIX = env.SERVER_HTTP_PREFIX || '/'
+var HTTP_PREFIX = env.SERVER_API_PREFIX || '/'
 var HTTP_HOST = env.SERVER_HOST || '0.0.0.0'
 var ENDPOINT = env.ENDPOINT_API || 'http://' + HTTP_HOST + ':' + HTTP_PORT
 
